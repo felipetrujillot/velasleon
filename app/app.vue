08e869449a0a93dc3bcdc86674e4f334883779df
Felipe Trujillo <trujillo.felipe1997@gmail.com>
2025-05-05 19:10:14 -0400

    avance

---
+++ app/app.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+/* useHead({
+  bodyAttrs: {
+    class: 'entel',
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/ico',
+    },
+  ],
+}) */
+</script>
+
+<template>
+  <div>
+    <NuxtLoadingIndicator />
+
+    <NuxtLayout>
+      <NuxtPage />
+      <Toaster />
+    </NuxtLayout>
+  </div>
+</template>
+
+<style>
+.container-sm {
+  width: 100%;
+  margin-right: auto;
+  margin-left: auto;
+  max-width: 960px;
+}
+
+.container-xs {
+  width: 100%;
+  margin-right: auto;
+  margin-left: auto;
+  max-width: 720px;
+}
+
+.container-xl {
+  width: 100%;
+  margin-right: auto;
+  margin-left: auto;
+  max-width: 1260px;
+}
+
+@keyframes fade {
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
+}
+
+.fadeIn {
+  animation: fade 0.3s ease-in-out;
+}
+
+.fadeInFast {
+  animation: fade 0.2s ease-in-out;
+}
+
+@keyframes bouncegentle {
+  0%,
+  100% {
+    transform: translateY(-10%);
+    animation-timing-function: cubic-bezier(0.6, 0, 0.8, 1);
+  }
+  50% {
+    transform: translateY(0);
+    animation-timing-function: cubic-bezier(0.2, 0, 0.4, 1);
+  }
+}
+
+.slow-gentle-bounce {
+  animation: bouncegentle 2s infinite;
+}
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-red-200">
+    <Button>Hola</Button>
+  </div>
+</template>
